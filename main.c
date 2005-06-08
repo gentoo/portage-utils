d227a18f5757a311e0bae5c546c08bd3bddb64f1
Mike Frysinger <vapier@gentoo.org>
2005-06-08 23:30:29 +0000

    define shared color codes, add qsize applet, move --nocolor/-C to common getopts level, move common getopt cases to a define, import make_human_readable_str from busybox, and move the ebuild cache file to a define

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  * Copyright 2005 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
- * $Header: /var/cvsroot/gentoo-projects/portage-utils/main.c,v 1.1 2005/06/07 04:37:32 vapier Exp $
+ * $Header: /var/cvsroot/gentoo-projects/portage-utils/main.c,v 1.2 2005/06/08 23:30:29 vapier Exp $
  *
  * 2005 Ned Ludd        - <solar@gentoo.org>
  * 2005 Mike Frysinger  - <vapier@gentoo.org>
@@ -64,6 +64,8 @@ static char *argv0;
 #define warn(fmt, args...) \
 	fprintf(stderr, "%s: " fmt "\n", argv0, ## args)
 #define warnf(fmt, args...) warn("%s(): " fmt, __FUNCTION__, ## args)
+#define warnp(fmt, args...) warn(fmt ": %s", ## args, strerror(errno))
+#define warnfp(fmt, args...) warnf(fmt ": %s", ## args, strerror(errno))
 #define err(fmt, args...) \
 	do { \
 	warn(fmt, ## args); \
@@ -78,7 +80,7 @@ static char *argv0;
 
 
 /* variables to control runtime behavior */
-static const char *rcsid = "$Id: main.c,v 1.1 2005/06/07 04:37:32 vapier Exp $";
+static const char *rcsid = "$Id: main.c,v 1.2 2005/06/08 23:30:29 vapier Exp $";
 
 static char color = 1;
 static char exact = 0;
@@ -89,12 +91,20 @@ static char portdir[_POSIX_PATH_MAX] = "/usr/portage";
 
 
 
+/* color constants */
+#define BOLD "\e[0;01m"
+#define BLUE "\e[36;01m"
+#define NORM "\e[0m"
+
+
+
 /* applet prototypes */
 int q_main(int, char **);
 int qsearch_main(int, char **);
 int quse_main(int, char **);
 int qlist_main(int, char **);
 int qfile_main(int, char **);
+int qsize_main(int, char **);
 
 /* applets we support */
 typedef enum {
@@ -102,7 +112,8 @@ typedef enum {
 	APPLET_QFILE = 1,
 	APPLET_QLIST = 2,
 	APPLET_QSEARCH = 3,
-	APPLET_QUSE = 4
+	APPLET_QUSE = 4,
+	APPLET_QSIZE = 5
 } applets_enum;
 struct applet_t {
 	const char *name;
@@ -116,30 +127,39 @@ struct applet_t {
 	{"qlist",   (APPLET)qlist_main,   "<pkgname>"},
 	{"qsearch", (APPLET)qsearch_main, "<regex>"},
 	{"quse",    (APPLET)quse_main,    "<useflag>"},
+	{"qsize",   (APPLET)qsize_main,   "<pkgname>"},
 	{NULL,      (APPLET)NULL,         NULL}
 };
 
 
 
 /* Common usage for all applets */
-#define COMMON_FLAGS "hV"
+#define COMMON_FLAGS "ChV"
 #define a_argument required_argument
 #define COMMON_LONG_OPTS \
+	{"nocolor",   no_argument, NULL, 'C'}, \
 	{"help",      no_argument, NULL, 'h'}, \
 	{"version",   no_argument, NULL, 'V'}, \
 	{NULL,        no_argument, NULL, 0x0}
 #define COMMON_OPTS_HELP \
+	"Don't output color", \
 	"Print this help and exit", \
 	"Print version and exit", \
 	NULL
+#define COMMON_GETOPTS_CASES(applet) \
+	case 'V': version_barf(); break; \
+	case 'h': applet ## _usage(EXIT_SUCCESS); break; \
+	case 'C': color = 0; break; \
+	default: applet ## _usage(EXIT_FAILURE); break;
+#define GETOPT_LONG(A, a, ex) \
+	getopt_long(argc, argv, ex A ## _FLAGS, a ## _long_opts, NULL)
 /* display usage and exit */
 static void usage(int status, const char *flags, struct option const opts[], 
                   const char *help[], applets_enum blabber)
 {
 	unsigned long i;
 	if (blabber == APPLET_Q) {
-		printf("Usage: q <applet> [arguments]...\n"
-		       "   or: <applet> [arguments]...\n\n");
+		printf("Usage: q <applet> [arguments]...\n\n");
 		printf("Currently defined applets:\n");
 		for (i = 0; applets[i].name; ++i)
 			printf(" - %s %s\n", applets[i].name, applets[i].opts);
@@ -147,7 +167,7 @@ static void usage(int status, const char *flags, struct option const opts[],
 		printf("Usage: %s %s\n", applets[blabber].name, applets[blabber].opts);
 	}
 
-	printf("\nOptions: [%s]\n", flags);
+	printf("\nOptions: -[%s]\n", flags);
 	for (i = 0; opts[i].name; ++i)
 		if (opts[i].has_arg == no_argument)
 			printf("  -%c, --%-13s* %s\n", opts[i].val, 
@@ -169,24 +189,88 @@ APPLET lookup_applet(char *applet)
 {
 	unsigned int i;
 	for (i = 0; applets[i].name; ++i) {
-		if ((strcmp(applets[i].name, applet)) == 0) {
+		if (strcmp(applets[i].name, applet) == 0) {
 			DBG("found applet %s at %p", applets[i].name, applets[i].func);
 			return applets[i].func;
 		}
 	}
 	/* No applet found? Search by shortname then... */
-	if ((strlen(applet)) - 1 > 0) {
+	if (strlen(applet) > 1) {
 		DBG("Looking up applet (%s) by short name", applet);
 		for (i = 1; applets[i].name; ++i) {
-			if ((strcmp(applets[i].name + 1, applet)) == 0) {
+			if (strcmp(applets[i].name + 1, applet) == 0) {
 				DBG("found applet by short name %s", applets[i].name);
 				return applets[i].func;
 			}
 		}
 	}
-	/* still nothing? .. add short opts -q/-l etc.. */
+	/* still nothing ?  those bastards ... */
 	warn("Unknown applet '%s'", applet);
 	return 0;
+}
+
+const char *make_human_readable_str(unsigned long long,unsigned long,unsigned long);
+enum {
+	KILOBYTE = 1024,
+	MEGABYTE = (KILOBYTE*1024),
+	GIGABYTE = (MEGABYTE*1024)
+};
+const char *make_human_readable_str(unsigned long long size,
+	unsigned long block_size, unsigned long display_unit)
+{
+	/* The code will adjust for additional (appended) units. */
+	static const char zero_and_units[] = { '0', 0, 'k', 'M', 'G', 'T' };
+	static const char fmt[] = "%Lu";
+	static const char fmt_tenths[] = "%Lu.%d%c";
+
+	static char str[21];		/* Sufficient for 64 bit unsigned integers. */
+
+	unsigned long long val;
+	int frac;
+	const char *u;
+	const char *f;
+
+	u = zero_and_units;
+	f = fmt;
+	frac = 0;
+
+	val = size * block_size;
+	if (val == 0) {
+		return u;
+	}
+
+	if (display_unit) {
+		val += display_unit/2;	/* Deal with rounding. */
+		val /= display_unit;	/* Don't combine with the line above!!! */
+	} else {
+		++u;
+		while ((val >= KILOBYTE)
+			   && (u < zero_and_units + sizeof(zero_and_units) - 1)) {
+			f = fmt_tenths;
+			++u;
+			frac = ((((int)(val % KILOBYTE)) * 10) + (KILOBYTE/2)) / KILOBYTE;
+			val /= KILOBYTE;
+		}
+		if (frac >= 10) {		/* We need to round up here. */
+			++val;
+			frac = 0;
+		}
+#if 0
+		/* Sample code to omit decimal point and tenths digit. */
+		if ( /* no_tenths */ 1 ) {
+			if ( frac >= 5 ) {
+				++val;
+			}
+			f = "%Lu%*c" /* fmt_no_tenths */ ;
+			frac = 1;
+		}
+#endif
+	}
+
+	/* If f==fmt then 'frac' and 'u' are ignored. */
+	snprintf(str, sizeof(str), f, val, frac, *u);
+
+	return str;
 }
 
 int rematch(char *regex, char *match, int cflags)
@@ -230,8 +314,8 @@ char *initialize_portdir(void)
 	size_t i;
 
 	if (p) {
-		if (strlen(p) < sizeof(portdir)) {
-			strncpy(portdir, p, sizeof(portdir));
+		if (strlen(p) + 1 < sizeof(portdir)) {
+			strcpy(portdir, p);
 			return portdir;
 		}
 	}
@@ -242,7 +326,7 @@ char *initialize_portdir(void)
 			if (strncmp(buf, "PORTDIR=", 8) != 0)
 				continue;
 			/* Sorry don't understand bash variables. */
-			if ((strchr(buf, '$')) != NULL)
+			if (strchr(buf, '$') != NULL)
 				continue;
 
 			for (i = 8; i < strlen(buf); i++)
@@ -257,6 +341,7 @@ char *initialize_portdir(void)
 	return portdir;
 }
 
+#define EBUILD_CACHE ".ebuild.x"
 void initialize_ebuild_flat(void)
 {
 	DIR *dir[3];
@@ -264,28 +349,25 @@ void initialize_ebuild_flat(void)
 	FILE *fp;
 	time_t start;
 
-	if ((chdir(portdir)) != 0) {
-		warn("Error: unable chdir to what I think is your PORTDIR '%s' : %s",
-		     portdir, strerror(errno));
+	if (chdir(portdir) != 0) {
+		warnp("chdir to PORTDIR '%s' failed", portdir);
 		return;
 	}
 
 	/* assuming --sync is used with --delete this will get recreated after every merged */
-	if (access(".ebuild.x", W_OK) == 0)
+	if (access(EBUILD_CACHE, W_OK) == 0)
 		return;
 
 	warn("Updating ebuild cache ... ");
 
-	unlink(".ebuild.x");
+	unlink(EBUILD_CACHE);
 	if (errno != ENOENT) {
-		warnf("Error: unlinking %s/%s : %s", portdir,
-		      ".ebuild.x", strerror(errno));
+		warnfp("unlinking '%s/%s' failed", portdir, EBUILD_CACHE);
 		return;
 	}
 
-	if ((fp = fopen(".ebuild.x", "w")) == NULL) {
-		warnf("Error opening %s/.ebuild.x %s", portdir,
-		      strerror(errno));
+	if ((fp = fopen(EBUILD_CACHE, "w")) == NULL) {
+		warnfp("opening '%s/%s' failed", portdir, EBUILD_CACHE);
 		return;
 	}
 
@@ -346,11 +428,10 @@ void initialize_ebuild_flat(void)
 void reinitialize_ebuild_flat(void)
 {
 	if ((chdir(portdir)) != 0) {
-		warn("Error: unable chdir to what I think is your PORTDIR '%s' : %s",
-		     portdir, strerror(errno));
+		warnp("chdir to PORTDIR '%s' failed", portdir);
 		return;
 	}
-	unlink(".ebuild.x");
+	unlink(EBUILD_CACHE);
 	initialize_ebuild_flat();
 }
 
@@ -364,6 +445,7 @@ void reinitialize_as_needed(void)
 #include "qlist.c"
 #include "qsearch.c"
 #include "quse.c"
+#include "qsize.c"
 #include "q.c"
 
 int main(int argc, char **argv)
