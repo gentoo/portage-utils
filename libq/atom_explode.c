843b53c8f79506338bc375c2eb9d69252f06008b
Mike Frysinger <vapier@gentoo.org>
2005-06-20 22:26:51 +0000

    handle atoms without categories

--- libq/atom_explode.c
+++ libq/atom_explode.c
@@ -1,7 +1,7 @@
 /*
  * Copyright 2005 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
- * $Header: /var/cvsroot/gentoo-projects/portage-utils/libq/atom_explode.c,v 1.4 2005/06/19 05:32:59 vapier Exp $
+ * $Header: /var/cvsroot/gentoo-projects/portage-utils/libq/atom_explode.c,v 1.5 2005/06/20 22:26:51 vapier Exp $
  *
  * 2005 Ned Ludd        - <solar@gentoo.org>
  * 2005 Mike Frysinger  - <vapier@gentoo.org>
@@ -37,6 +37,8 @@ depend_atom *atom_explode(const char *atom)
 	depend_atom *ret;
 	char *ptr, *ptr_tmp;
 	size_t len, slen;
+	int i;
+	const char *suffixes[] = { "_alpha", "_beta", "_pre", "_rc", "_p", NULL };
 
 	/* we allocate mem for atom struct and two strings (strlen(atom)).
 	 * the first string is for CAT/PN/PV while the second is for PVR */
@@ -49,101 +51,102 @@ depend_atom *atom_explode(const char *atom)
 	ret->CATEGORY = ret->PVR + slen + 1;
 	memcpy(ret->CATEGORY, atom, slen);
 
-	/* this shit looks scary huh BUT YOU LIKE IT */
+	/* break up the CATEOGRY and PVR */
 	if ((ptr = strrchr(ret->CATEGORY, '/')) != NULL) {
-		int i;
-		const char *suffixes[] = { "_alpha", "_beta", "_pre", "_rc", "_p", NULL };
-
-		/* break off the PVR from the CATEGORY */
 		ret->PN = ptr+1;
 		*ptr = '\0';
-
 		/* eat extra crap in case it exists */
-		if ((ptr_tmp = strrchr(ret->CATEGORY, '/')) != NULL)
-			ret->CATEGORY = ptr_tmp+1;
+		if ((ptr = strrchr(ret->CATEGORY, '/')) != NULL)
+			ret->CATEGORY = ptr+1;
+	} else {
+		ret->PN = ret->CATEGORY;
+		ret->CATEGORY = NULL;
+	}
 
-		/* search for the special suffixes */
-		for (i = 0; i >= 0 && suffixes[i]; ++i) {
-			ptr_tmp = ret->PN;
+	/* eat file name crap */
+	if ((ptr = strstr(ret->PN, ".ebuild")) != NULL)
+		*ptr = '\0';
+
+	/* search for the special suffixes */
+	for (i = 0; i >= 0 && suffixes[i]; ++i) {
+		ptr_tmp = ret->PN;
 
 retry_suffix:
-			if ((ptr = strstr(ptr_tmp, suffixes[i])) != NULL) {
-				/* check this is a real suffix and not _p hitting mod_perl */
-				/* note: '_suff-' in $PN is accepted, but no one uses that ... */
-				len = strlen(ptr);
-				slen = strlen(suffixes[i]);
-				if (slen > len) continue;
-				if (ptr[slen] && !isdigit(ptr[slen]) && ptr[slen]!='-') {
-					/* ok, it was a fake out ... lets skip this 
-					 * fake and try to match the suffix again */
-					ptr_tmp = ptr + 1;
-					goto retry_suffix;
-				}
+		if ((ptr = strstr(ptr_tmp, suffixes[i])) != NULL) {
+			/* check this is a real suffix and not _p hitting mod_perl */
+			/* note: '_suff-' in $PN is accepted, but no one uses that ... */
+			len = strlen(ptr);
+			slen = strlen(suffixes[i]);
+			if (slen > len) continue;
+			if (ptr[slen] && !isdigit(ptr[slen]) && ptr[slen]!='-') {
+				/* ok, it was a fake out ... lets skip this 
+				 * fake and try to match the suffix again */
+				ptr_tmp = ptr + 1;
+				goto retry_suffix;
+			}
 
 eat_version:
-				/* allow for 1 optional suffix letter */
-				if (ptr[-1] >= 'a' && ptr[-1] <= 'z') {
-					ptr_tmp = ptr--;
-					while (--ptr > ret->PN)
-						if (*ptr != '.' && !isdigit(*ptr))
-							break;
-					if (*ptr != '-') {
-						ptr = ptr_tmp;
-					}
-				}
-
-				/* eat the trailing version number [-.0-9]+ */
-				ptr_tmp = ptr;
+			/* allow for 1 optional suffix letter */
+			if (ptr[-1] >= 'a' && ptr[-1] <= 'z') {
+				ptr_tmp = ptr--;
 				while (--ptr > ret->PN)
-					if (*ptr == '-') {
-						ptr_tmp = ptr;
-						continue;
-					} else if (*ptr != '-' && *ptr != '.' && !isdigit(*ptr)) {
-						ret->PV = ptr_tmp+1;
-						ret->PV[-1] = '\0';
-						goto found_pv;
-					}
-				ret->PV = ptr_tmp+1;
-				*ptr_tmp = '\0';
-				break;
+					if (*ptr != '.' && !isdigit(*ptr))
+						break;
+				if (*ptr != '-') {
+					ptr = ptr_tmp;
+				}
 			}
+
+			/* eat the trailing version number [-.0-9]+ */
+			ptr_tmp = ptr;
+			while (--ptr > ret->PN)
+				if (*ptr == '-') {
+					ptr_tmp = ptr;
+					continue;
+				} else if (*ptr != '-' && *ptr != '.' && !isdigit(*ptr)) {
+					ret->PV = ptr_tmp+1;
+					ret->PV[-1] = '\0';
+					goto found_pv;
+				}
+			ret->PV = ptr_tmp+1;
+			*ptr_tmp = '\0';
+			break;
 		}
-		if (i <= -3)
-			errf("Hrm, seem to have hit an infinite loop with %s", atom);
+	}
+	if (i <= -3)
+		errf("Hrm, seem to have hit an infinite loop with %s", atom);
 
 found_pv:
-		i = -1;
-		if (ret->PV) {
-			/* if we got the PV, split the -r# off */
-			if ((ptr = strstr(ret->PV, "-r")) != NULL) {
-				ret->PR_int = atoi(ptr+2);
-				strcpy(ret->PVR, ret->PV);
-				*ptr = '\0';
-			} else {
-				ret->PR_int = 0;
-				sprintf(ret->PVR, "%s-r0", ret->PV);
-			}
+	i = -1;
+	if (ret->PV) {
+		/* if we got the PV, split the -r# off */
+		if ((ptr = strstr(ret->PV, "-r")) != NULL) {
+			ret->PR_int = atoi(ptr+2);
+			strcpy(ret->PVR, ret->PV);
+			*ptr = '\0';
 		} else {
-			/* this means that we couldn't match any of the special suffixes,
-			 * so we eat the -r# suffix (if it exists) before we throw the
-			 * ptr back into the version eater code above
-			 */
-			ptr_tmp = ptr = ret->PN + strlen(ret->PN) - 1;
-			do {
-				if (!isdigit(*ptr))
-					break;
-			} while (--ptr > ret->PN);
-			/* if we did find a -r#, eat the 'r', otherwise
-			 * reset ourselves to the end of the version # */
-			if (*ptr == 'r')
-				--ptr;
-			else
-				ptr = ptr_tmp;
-			--i;
-			goto eat_version;
+			ret->PR_int = 0;
+			sprintf(ret->PVR, "%s-r0", ret->PV);
 		}
-	} else
-		errf("Could not locate a / in '%s'", atom);
+	} else {
+		/* this means that we couldn't match any of the special suffixes,
+		 * so we eat the -r# suffix (if it exists) before we throw the
+		 * ptr back into the version eater code above
+		 */
+		ptr_tmp = ptr = ret->PN + strlen(ret->PN) - 1;
+		do {
+			if (!isdigit(*ptr))
+				break;
+		} while (--ptr > ret->PN);
+		/* if we did find a -r#, eat the 'r', otherwise
+		 * reset ourselves to the end of the version # */
+		if (*ptr == 'r')
+			--ptr;
+		else
+			ptr = ptr_tmp;
+		--i;
+		goto eat_version;
+	}
 
 	return ret;
 }
