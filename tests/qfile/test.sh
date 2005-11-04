f218bf6e18eacf83324a873061b2a43a67bb533e
Ned Ludd <solar@gentoo.org>
2005-11-04 04:12:49 +0000

    - remote test.sh

--- tests/qfile/test.sh
+++
@@ -1,9 +0,0 @@
-
-QFILE='../../q file'
-if [[ $(${QFILE} -C /bin/bash)  != "app-shells/bash (/bin/bash)" ]]; then
-	echo "/bin/bash does not seem to be provided by app-shells/bash. Found $(${QFILE} -Cq /bin/bash)" > /dev/stderr
-	echo FAILED
-	exit 1
-fi
-echo PASSED
-exit 0
