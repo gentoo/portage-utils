a78eb261583d5e7b7a383d735d11b058af17f225
Ned Ludd <solar@gentoo.org>
2005-11-14 01:16:27 +0000

    - update example template to shell proper use of applet_rcsid[]

--- template.c
+++ template.c
@@ -1,7 +1,7 @@
 /*
  * Copyright 2005 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
- * $Header: /var/cvsroot/gentoo-projects/portage-utils/template.c,v 1.3 2005/11/06 18:35:50 solar Exp $
+ * $Header: /var/cvsroot/gentoo-projects/portage-utils/template.c,v 1.4 2005/11/14 01:16:27 solar Exp $
  *
  * Copyright 2005 Ned Ludd        - <solar@gentoo.org>
  * Copyright 2005 Mike Frysinger  - <vapier@gentoo.org>
@@ -15,7 +15,7 @@ static const char *qtemp_opts_help[] = {
 	COMMON_OPTS_HELP
 };
 
-#define QTEMP_RCSID "$Id: template.c,v 1.3 2005/11/06 18:35:50 solar Exp $"
+static char qtemp_rcsid[] = "$Id: template.c,v 1.4 2005/11/14 01:16:27 solar Exp $";
 #define qtemp_usage(ret) usage(ret, QTEMP_FLAGS, qtemp_long_opts, qtemp_opts_help, APPLET_QTEMP)
 
 
