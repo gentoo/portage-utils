be80ec847254932fa06f4183cbc1ba6c7f05cc1a
Mike Frysinger <vapier@gentoo.org>
2006-01-05 03:15:09 +0000

    make the output a little nicer

--- qlop.c
+++ qlop.c
@@ -1,10 +1,10 @@
 /*
- * Copyright 2005 Gentoo Foundation
+ * Copyright 2005-2006 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
- * $Header: /var/cvsroot/gentoo-projects/portage-utils/qlop.c,v 1.25 2005/12/30 08:06:59 vapier Exp $
+ * $Header: /var/cvsroot/gentoo-projects/portage-utils/qlop.c,v 1.26 2006/01/05 03:15:09 vapier Exp $
  *
- * Copyright 2005 Ned Ludd        - <solar@gentoo.org>
- * Copyright 2005 Mike Frysinger  - <vapier@gentoo.org>
+ * Copyright 2005-2006 Ned Ludd        - <solar@gentoo.org>
+ * Copyright 2005-2006 Mike Frysinger  - <vapier@gentoo.org>
  */
 
 #ifdef __linux__
@@ -39,7 +39,7 @@ static const char *qlop_opts_help[] = {
 	"Read emerge logfile instead of " QLOP_DEFAULT_LOGFILE,
 	COMMON_OPTS_HELP
 };
-static const char qlop_rcsid[] = "$Id: qlop.c,v 1.25 2005/12/30 08:06:59 vapier Exp $";
+static const char qlop_rcsid[] = "$Id: qlop.c,v 1.26 2006/01/05 03:15:09 vapier Exp $";
 #define qlop_usage(ret) usage(ret, QLOP_FLAGS, qlop_long_opts, qlop_opts_help, lookup_applet_idx("qlop"))
 
 #define QLOP_LIST    0x01
@@ -127,12 +127,13 @@ unsigned long show_merge_times(char *pkg, const char *logfile, int average, char
 						break;
 					if ((strncmp(buf[1], "::: completed emerge (", 22)) == 0) {
 						if (!average) {
-							printf("%s%s%s: %lu ",
-								BLUE, atom->PN, NORM, (t[1] - t[0]));
+							printf("%s%s%s: %s: ",
+								BLUE, atom->PN, NORM, chop_ctime(t[0]));
 							if (human_readable)
 								print_seconds_for_earthlings(t[1] - t[0]);
-							printf(" %s", chop_ctime(t[0]));
-							puts(NORM);
+							else
+								printf("%s%lu%s seconds", GREEN, (t[1] - t[0]), NORM);
+							puts("");
 						}
 						merge_time += (t[1] - t[0]);
 						count++;
@@ -151,11 +152,11 @@ unsigned long show_merge_times(char *pkg, const char *logfile, int average, char
 		if (human_readable)
 			print_seconds_for_earthlings(merge_time / count);
 		else
-			printf("%lu seconds average", merge_time / count);
-		printf(" for %lu merges\n", count);
-		return 0;
+			printf("%s%lu%s seconds average", GREEN, merge_time / count, NORM);
+		printf(" for %s%lu%s merges\n", GREEN, count, NORM);
+	} else {
+		printf("%s%s%s: %s%lu%s times\n", BLUE, pkg, NORM, GREEN, count, NORM);
 	}
-	printf("%s%s%s: %lu times\n", BLUE, pkg, NORM, count);
 	return 0;
 }
 
