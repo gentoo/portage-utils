afdcdf8aeb7d5b1c178350a1801d7667bd993c9b
Ned Ludd <solar@gentoo.org>
2007-04-05 18:23:30 +0000

    - src.style fixups

--- qgrep.c
+++ qgrep.c
@@ -1,7 +1,7 @@
 /*
  * Copyright 2005-2006 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
- * $Header: /var/cvsroot/gentoo-projects/portage-utils/qgrep.c,v 1.21 2007/04/02 17:05:04 solar Exp $
+ * $Header: /var/cvsroot/gentoo-projects/portage-utils/qgrep.c,v 1.22 2007/04/05 18:23:30 solar Exp $
  *
  * Copyright 2005-2006 Ned Ludd        - <solar@gentoo.org>
  * Copyright 2005-2006 Mike Frysinger  - <vapier@gentoo.org>
@@ -47,7 +47,7 @@ static const char *qgrep_opts_help[] = {
 	"Print <arg> lines of trailing context",
 	COMMON_OPTS_HELP
 };
-static const char qgrep_rcsid[] = "$Id: qgrep.c,v 1.21 2007/04/02 17:05:04 solar Exp $";
+static const char qgrep_rcsid[] = "$Id: qgrep.c,v 1.22 2007/04/05 18:23:30 solar Exp $";
 #define qgrep_usage(ret) usage(ret, QGREP_FLAGS, qgrep_long_opts, qgrep_opts_help, lookup_applet_idx("qgrep"))
 
 char qgrep_name_match(const char*, const int, depend_atom**);
@@ -208,9 +208,8 @@ void qgrep_print_before_context(qgrep_buf_t *current, const char num_lines_befor
 }
 
 /* Yield the path of one of the installed ebuilds (from VDB). */
-char* get_next_installed_ebuild(char *, DIR *, struct dirent **, DIR **);
-char* get_next_installed_ebuild(char *ebuild_path, DIR *vdb_dir, 
-		struct dirent **cat_dirent_pt, DIR **cat_dir_pt)
+char *get_next_installed_ebuild(char *, DIR *, struct dirent **, DIR **);
+char *get_next_installed_ebuild(char *ebuild_path, DIR *vdb_dir, struct dirent **cat_dirent_pt, DIR **cat_dir_pt)
 {
 	struct dirent *pkg_dirent = NULL;
 	if (*cat_dirent_pt == NULL || *cat_dir_pt == NULL)
