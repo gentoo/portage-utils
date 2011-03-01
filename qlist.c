56747feec6e32cbd598de684c458b06c05817bb7
Mike Frysinger <vapier@gentoo.org>
2011-03-01 05:17:13 +0000

    dont bother checking for hidden dirs again since filter_hidden did this already

--- qlist.c
+++ qlist.c
@@ -1,7 +1,7 @@
 /*
  * Copyright 2005-2010 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
- * $Header: /var/cvsroot/gentoo-projects/portage-utils/qlist.c,v 1.60 2011/03/01 05:01:30 vapier Exp $
+ * $Header: /var/cvsroot/gentoo-projects/portage-utils/qlist.c,v 1.61 2011/03/01 05:17:13 vapier Exp $
  *
  * Copyright 2005-2010 Ned Ludd        - <solar@gentoo.org>
  * Copyright 2005-2010 Mike Frysinger  - <vapier@gentoo.org>
@@ -41,7 +41,7 @@ static const char * const qlist_opts_help[] = {
 	/* "query filename for pkgname", */
 	COMMON_OPTS_HELP
 };
-static const char qlist_rcsid[] = "$Id: qlist.c,v 1.60 2011/03/01 05:01:30 vapier Exp $";
+static const char qlist_rcsid[] = "$Id: qlist.c,v 1.61 2011/03/01 05:17:13 vapier Exp $";
 #define qlist_usage(ret) usage(ret, QLIST_FLAGS, qlist_long_opts, qlist_opts_help, lookup_applet_idx("qlist"))
 
 extern char *grab_vdb_item(const char *, const char *, const char *);
@@ -209,7 +209,7 @@ int qlist_main(int argc, char **argv)
 			FILE *fp;
 			static char *slotted_item, *slot;
 
-			if (de[x]->d_name[0] == '.' || de[x]->d_name[0] == '-')
+			if (de[x]->d_name[0] == '-')
 				continue;
 
 			slot = slotted_item = NULL;
