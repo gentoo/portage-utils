7bebd358c61675254d5a1199b5cb1ee783813c40
Mike Frysinger <vapier@gentoo.org>
2011-03-01 04:58:39 +0000

    touch up style a bit

--- libq/which.c
+++ libq/which.c
@@ -2,19 +2,24 @@
 static char *which(const char *fname)
 {
 	static char fullpath[BUFSIZ];
-	char *path, *p;
+	char *ret, *path, *p;
+
+	ret = NULL;
+
 	path = getenv("PATH");
 	if (!path)
-		return NULL;
+		return ret;
+
 	path = xstrdup(path);
 	while ((p = strrchr(path, ':')) != NULL) {
 		snprintf(fullpath, sizeof(fullpath), "%s/%s", p + 1, fname);
 		*p = 0;
-		if (access(fullpath, R_OK) != (-1)) {
-			free(path);
-			return (char *) fullpath;
+		if (access(fullpath, R_OK) != -1) {
+			ret = fullpath;
+			break;
 		}
 	}
 	free(path);
-	return NULL;
+
+	return ret;
 }
