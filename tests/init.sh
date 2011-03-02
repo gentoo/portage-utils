67cb5f88a7c8f2833bf2946c64b400495b83f142
Mike Frysinger <vapier@gentoo.org>
2011-03-02 01:19:27 +0000

    add test for mkdir_p/rm_rf code

--- tests/init.sh
+++ tests/init.sh
@@ -18,3 +18,9 @@ pass() {
 	echo "PASSED"
 	exit 0
 }
+
+mktmpdir() {
+	local d=${1:-tmp}
+	rm -rf "$d" && mkdir "$d" && cd "$d" \
+		|| fail "could not make tmp dir '$1'"
+}
---
+++ tests/mkdir/test.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2005-2008 Gentoo Foundation
+ * Distributed under the terms of the GNU General Public License v2
+ * $Header: /var/cvsroot/gentoo-projects/portage-utils/tests/mkdir/test.c,v 1.1 2011/03/02 01:19:27 vapier Exp $
+ *
+ * Copyright 2005-2008 Ned Ludd        - <solar@gentoo.org>
+ * Copyright 2005-2008 Mike Frysinger  - <vapier@gentoo.org>
+ */
+
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <getopt.h>
+#include <regex.h>
+#include <errno.h>
+#include <libgen.h>
+#include <limits.h>
+#include <assert.h>
+
+#define warnf(fmt, args...) fprintf(stderr, fmt "\n", ## args)
+#define errf(fmt, args...) \
+	do { \
+	warnf(fmt, ## args); \
+	exit(EXIT_FAILURE); \
+	} while (0)
+#define err(...) errf(__VA_ARGS__)
+#define errp(...) errf(__VA_ARGS__)
+
+#include "../../libq/xmalloc.c"
+#include "../../libq/xstrdup.c"
+#include "../../libq/xmkdir.c"
+
+int main(int argc, char *argv[])
+{
+	int i, ret;
+
+	if (argc <= 1)
+		return 1;
+
+	ret = 0;
+
+	if (!strcmp(argv[1], "m")) {
+		for (i = 2; i < argc; ++i)
+			ret += mkdir_p(argv[i], 0755);
+	} else if (!strcmp(argv[1], "rm")) {
+		for (i = 2; i < argc; ++i)
+			ret += rm_rf(argv[i]);
+	}
+
+	return ret;
+}
