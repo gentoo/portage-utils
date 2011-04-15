9f88c840668d8baa3533862614df669ca4faa1cf
Mike Frysinger <vapier@gentoo.org>
2011-04-15 23:09:56 +0000

    add O_CLOEXEC fallback define #363271

--- libq/compat.c
+++ libq/compat.c
@@ -51,3 +51,8 @@ char* strcasestr(const char *big, const char *little) {
 #ifdef __hpux
 # define S_BLKSIZE st.st_blksize
 #endif
+
+/* Older systems */
+#ifndef O_CLOEXEC
+# define O_CLOEXEC 0
+#endif
