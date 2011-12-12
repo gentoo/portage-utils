199a434011e3d7511791aac55854a8da3b67ffb9
Mike Frysinger <vapier@gentoo.org>
2011-12-12 21:32:19 +0000

    add checking support

--- make-tarball.sh
+++ make-tarball.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+v() { printf '\t%s\n' "$*"; "$@"; }
+
+: ${MAKE:=make}
+
 if [[ $# -ne 1 ]] ; then
 	echo "Usage: $0 <ver>" 1>&2
 	exit 1
@@ -17,10 +21,12 @@ mkdir "${p}"
 echo "Checking out clean cvs sources ..."
 cp -a CVS "${p}"/
 cd "${p}"
-cvs -Q up
+v cvs -Q up
+
+echo "Building autotools ..."
 sed -i "/^AC_INIT/s:cvs:${ver}:" configure.ac
 sed -i "1iPV := ${ver}" Makefile
-LC_ALL=C ${MAKE:-make} autotools
+LC_ALL=C ${MAKE} -s autotools >/dev/null
 rm -rf autom4te.cache
 cd ..
 
@@ -29,5 +35,22 @@ find "${p}" -type d -name CVS -prune -print0 | xargs -0 rm -rf
 tar cf - "${p}" | xz > "${p}".tar.xz
 rm -r "${p}"
 du -b "${p}".tar.*
+
+echo "Checking tarball (simple) ..."
+tar xf "${p}".tar.*
+pushd "${p}" >/dev/null
+v ${MAKE} -s
+v ${MAKE} -s check
+popd >/dev/null
+rm -rf "${p}"
+
+echo "Checking tarball (autotools) ..."
+tar xf "${p}".tar.*
+pushd "${p}" >/dev/null
+v ./configure -q
+v ${MAKE} -s
+v ${MAKE} -s check
+popd >/dev/null
+rm -rf "${p}"
 
 exit 0
