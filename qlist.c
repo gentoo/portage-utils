535c8974445b88e38c7003fb53b8899a321b66de
Mike Frysinger <vapier@gentoo.org>
2011-12-18 07:27:37 +0000

    use new q_vdb_foreach_pkg_sorted helper

--- qlist.c
+++ qlist.c
@@ -1,7 +1,7 @@
 /*
  * Copyright 2005-2010 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
- * $Header: /var/cvsroot/gentoo-projects/portage-utils/qlist.c,v 1.64 2011/12/18 06:31:29 vapier Exp $
+ * $Header: /var/cvsroot/gentoo-projects/portage-utils/qlist.c,v 1.65 2011/12/18 07:27:37 vapier Exp $
  *
  * Copyright 2005-2010 Ned Ludd        - <solar@gentoo.org>
  * Copyright 2005-2010 Mike Frysinger  - <vapier@gentoo.org>
@@ -41,7 +41,7 @@ static const char * const qlist_opts_help[] = {
 	/* "query filename for pkgname", */
 	COMMON_OPTS_HELP
 };
-static const char qlist_rcsid[] = "$Id: qlist.c,v 1.64 2011/12/18 06:31:29 vapier Exp $";
+static const char qlist_rcsid[] = "$Id: qlist.c,v 1.65 2011/12/18 07:27:37 vapier Exp $";
 #define qlist_usage(ret) usage(ret, QLIST_FLAGS, qlist_long_opts, qlist_opts_help, lookup_applet_idx("qlist"))
 
 queue *filter_dups(queue *sets);
@@ -79,7 +79,7 @@ _q_static char *q_vdb_pkg_eat(q_vdb_pkg_ctx *pkg_ctx, const char *item)
 	return buf;
 }
 
-static char *grab_pkg_umap(char *CAT, char *PV)
+static char *grab_pkg_umap(const char *CAT, const char *PV)
 {
 	static char umap[BUFSIZ];
 	char *use = NULL;
@@ -134,7 +134,7 @@ static char *grab_pkg_umap(char *CAT, char *PV)
 	return umap;
 }
 
-static const char *umapstr(char display, char *cat, char *name)
+static const char *umapstr(char display, const char *cat, const char *name)
 {
 	static char buf[BUFSIZ];
 	char *umap = NULL;
@@ -151,208 +151,200 @@ static const char *umapstr(char display, char *cat, char *name)
 	return buf;
 }
 
+struct qlist_opt_state {
+	int argc;
+	char **argv;
+	bool exact;
+	bool all;
+	bool just_pkgname;
+	bool dups_only;
+	bool show_dir;
+	bool show_obj;
+	bool show_sym;
+	bool show_slots;
+	bool show_umap;
+	bool columns;
+	const char *slot_separator;
+	queue *sets;
+	char *buf;
+	size_t buflen;
+};
+
+_q_static int qlist_cb(q_vdb_pkg_ctx *pkg_ctx, void *priv)
+{
+	struct qlist_opt_state *state = priv;
+	int i;
+	FILE *fp;
+	static char *slotted_item, *slot;
+	const char *catname = pkg_ctx->cat_ctx->name;
+	const char *pkgname = pkg_ctx->name;
+	depend_atom *atom;
+	char swap[_Q_PATH_MAX];
+
+	slot = slotted_item = NULL;
+
+	/* see if this cat/pkg is requested */
+	for (i = optind; i < state->argc; ++i) {
+		char *name = pkg_name(state->argv[i]);
+		snprintf(state->buf, state->buflen, "%s/%s", catname, pkgname);
+		/* printf("buf=%s:%s\n", buf,grab_vdb_item("SLOT", catname, pkgname)); */
+		if (state->exact) {
+			if ((atom = atom_explode(state->buf)) == NULL) {
+				warn("invalid atom %s", state->buf);
+				continue;
+			}
+			snprintf(swap, sizeof(swap), "%s/%s",
+				 atom->CATEGORY, atom->PN);
+			atom_implode(atom);
+			if ((strcmp(name, swap) == 0) || (strcmp(name, state->buf) == 0))
+				break;
+			if ((strcmp(name, strstr(swap, "/") + 1) == 0) || (strcmp(name, strstr(state->buf, "/") + 1) == 0))
+				break;
+		} else {
+			if (charmatch(name, state->buf) == 0)
+				break;
+			if (charmatch(name, pkgname) == 0)
+				break;
+			if (rematch(name, state->buf, REG_EXTENDED) == 0)
+				break;
+			if (rematch(name, pkgname, REG_EXTENDED) == 0)
+				break;
+		}
+	}
+	if ((i == state->argc) && (state->argc != optind))
+		return 0;
+
+	if (i < state->argc) {
+		if ((slotted_item = slot_name(state->argv[i])) != NULL) {
+			slot = q_vdb_pkg_eat(pkg_ctx, "SLOT");
+			if (strcmp(slotted_item, slot) != 0)
+				return 0;
+		}
+	}
+	if (state->just_pkgname) {
+		if (state->dups_only) {
+			atom = atom_explode(pkgname);
+			snprintf(state->buf, state->buflen, "%s/%s", catname, atom->P);
+			snprintf(swap, sizeof(swap), "%s/%s", catname, atom->PN);
+			state->sets = add_set(swap, state->buf, state->sets);
+			atom_implode(atom);
+			return 0;
+		}
+		atom = (verbose ? NULL : atom_explode(pkgname));
+		if ((state->all + state->just_pkgname) < 2) {
+
+			slot = NULL;
+
+			if (state->show_slots)
+				slot = q_vdb_pkg_eat(pkg_ctx, "SLOT");
+
+			/* display it */
+			printf("%s%s/%s%s%s%s%s%s%s%s%s%s\n", BOLD, catname, BLUE,
+				(!state->columns ? (atom ? atom->PN : pkgname) : atom->PN),
+				(state->columns ? " " : ""), (state->columns ? atom->PV : ""),
+				NORM, YELLOW, slot ? state->slot_separator : "", slot ? slot : "", NORM,
+				umapstr(state->show_umap, catname, pkgname));
+		}
+		if (atom)
+			atom_implode(atom);
+
+		if (!state->all)
+			return 0;
+	}
+
+	if (verbose > 1)
+		printf("%s%s/%s%s%s\n%sCONTENTS%s:\n", BOLD, catname, BLUE, pkgname, NORM, DKBLUE, NORM);
+
+	fp = q_vdb_pkg_fopenat_ro(pkg_ctx, "CONTENTS");
+	if (fp == NULL)
+		return 0;
+
+	while (getline(&state->buf, &state->buflen, fp) != -1) {
+		contents_entry *e;
+
+		e = contents_parse_line(state->buf);
+		if (!e)
+			continue;
+
+		switch (e->type) {
+			case CONTENTS_DIR:
+				if (state->show_dir)
+					printf("%s%s%s/\n", verbose > 1 ? YELLOW : "" , e->name, verbose > 1 ? NORM : "");
+				break;
+			case CONTENTS_OBJ:
+				if (state->show_obj)
+					printf("%s%s%s\n", verbose > 1 ? WHITE : "" , e->name, verbose > 1 ? NORM : "");
+				break;
+			case CONTENTS_SYM:
+				if (state->show_sym) {
+					if (verbose)
+						printf("%s%s -> %s%s\n", verbose > 1 ? CYAN : "", e->name, e->sym_target, NORM);
+					else
+						printf("%s\n", e->name);
+				}
+				break;
+		}
+	}
+	fclose(fp);
+
+	return 0;
+}
+
 int qlist_main(int argc, char **argv)
 {
-	q_vdb_ctx *ctx;
-	q_vdb_cat_ctx *cat_ctx;
-	q_vdb_pkg_ctx *pkg_ctx;
-	int i, j, dfd, ret;
-	char qlist_all = 0, just_pkgname = 0, dups_only = 0;
-	char show_dir, show_obj, show_sym, show_slots, show_umap;
-	struct dirent **de, **cat;
-	size_t buflen;
-	char *buf;
-	char swap[_Q_PATH_MAX];
-	queue *sets = NULL;
-	depend_atom *pkgname, *atom;
-	const char *slot_separator;
-	int columns = 0;
-
-	slot_separator = " ";
+	struct qlist_opt_state state = {
+		.argc = argc,
+		.argv = argv,
+		.exact = false,
+		.all = false,
+		.just_pkgname = false,
+		.dups_only = false,
+		.show_dir = false,
+		.show_obj = false,
+		.show_sym = false,
+		.show_slots = false,
+		.show_umap = false,
+		.columns = false,
+		.slot_separator = " ",
+		.sets = NULL,
+		.buflen = _Q_PATH_MAX,
+	};
+	int i, ret;
 
 	DBG("argc=%d argv[0]=%s argv[1]=%s",
 	    argc, argv[0], argc > 1 ? argv[1] : "NULL?");
 
-	show_dir = show_obj = show_sym = show_slots = show_umap = 0;
-
 	while ((i = GETOPT_LONG(QLIST, qlist, "")) != -1) {
 		switch (i) {
 		COMMON_GETOPTS_CASES(qlist)
-		case 'a': qlist_all = 1;
-		case 'I': just_pkgname = 1; break;
-		case 'L': slot_separator = ":"; break;
-		case 'S': just_pkgname = 1; show_slots = 1; break;
-		case 'U': just_pkgname = 1; show_umap = 1; break;
-		case 'e': exact = 1; break;
-		case 'd': show_dir = 1; break;
-		case 'o': show_obj = 1; break;
-		case 's': show_sym = 1; break;
-		case 'D': dups_only = 1; exact = 1; just_pkgname = 1; break;
-		case 'c': columns = 1; break;
+		case 'a': state.all = true;
+		case 'I': state.just_pkgname = true; break;
+		case 'L': state.slot_separator = ":"; break;
+		case 'S': state.just_pkgname = state.show_slots = true; break;
+		case 'U': state.just_pkgname = state.show_umap = true; break;
+		case 'e': state.exact = true; break;
+		case 'd': state.show_dir = true; break;
+		case 'o': state.show_obj = true; break;
+		case 's': state.show_sym = true; break;
+		case 'D': state.dups_only = state.exact = state.just_pkgname = true; break;
+		case 'c': state.columns = true; break;
 		case 'f': break;
 		}
 	}
-	if (columns) verbose = 0; /* if not set to zero; atom wont be exploded; segv */
+	if (state.columns) verbose = 0; /* if not set to zero; atom wont be exploded; segv */
 	/* default to showing syms and objs */
-	if (!show_dir && !show_obj && !show_sym)
-		show_obj = show_sym = 1;
-	if ((argc == optind) && (!just_pkgname))
+	if (!state.show_dir && !state.show_obj && !state.show_sym)
+		state.show_obj = state.show_sym = true;
+	if (argc == optind && !state.just_pkgname)
 		qlist_usage(EXIT_FAILURE);
 
-	buflen = _Q_PATH_MAX;
-	buf = xmalloc(buflen);
-	ret = EXIT_FAILURE;
+	state.buf = xmalloc(state.buflen);
+	ret = q_vdb_foreach_pkg_sorted(qlist_cb, &state);
 
-	ctx = q_vdb_open();
-	if (!ctx)
-		return EXIT_FAILURE;
-
-	if ((dfd = scandirat(ctx->vdb_fd, ".", &cat, q_vdb_filter_cat, alphasort)) < 0)
-		goto done;
-
-	/* open /var/db/pkg */
-	for (j = 0; j < dfd; j++) {
-		int a, x;
-
-		/* open the cateogry */
-		cat_ctx = q_vdb_open_cat(ctx, cat[j]->d_name);
-		if (!cat_ctx)
-			continue;
-
-		a = scandirat(ctx->vdb_fd, cat[j]->d_name, &de, q_vdb_filter_pkg, alphasort);
-		if (a < 0) {
-			q_vdb_close_cat(cat_ctx);
-			continue;
-		}
-
-		for (x = 0; x < a; x++) {
-			FILE *fp;
-			static char *slotted_item, *slot;
-
-			if (de[x]->d_name[0] == '-')
-				continue;
-
-			pkg_ctx = q_vdb_open_pkg(cat_ctx, de[x]->d_name);
-			if (!pkg_ctx)
-				continue;
-
-			slot = slotted_item = NULL;
-
-			/* see if this cat/pkg is requested */
-			for (i = optind; i < argc; ++i) {
-				char *name = pkg_name(argv[i]);
-				snprintf(buf, buflen, "%s/%s", cat[j]->d_name, de[x]->d_name);
-				/* printf("buf=%s:%s\n", buf,grab_vdb_item("SLOT", cat[j]->d_name, de[x]->d_name)); */
-				if (exact) {
-					if ((atom = atom_explode(buf)) == NULL) {
-						warn("invalid atom %s", buf);
-						continue;
-					}
-					snprintf(swap, sizeof(swap), "%s/%s",
-						 atom->CATEGORY, atom->PN);
-					atom_implode(atom);
-					if ((strcmp(name, swap) == 0) || (strcmp(name, buf) == 0))
-						break;
-					if ((strcmp(name, strstr(swap, "/") + 1) == 0) || (strcmp(name, strstr(buf, "/") + 1) == 0))
-						break;
-				} else {
-					if (charmatch(name, buf) == 0)
-						break;
-					if (charmatch(name, de[x]->d_name) == 0)
-						break;
-					if (rematch(name, buf, REG_EXTENDED) == 0)
-						break;
-					if (rematch(name, de[x]->d_name, REG_EXTENDED) == 0)
-						break;
-				}
-			}
-			if ((i == argc) && (argc != optind))
-				goto next_cat;
-
-			if (i < argc) {
-				if ((slotted_item = slot_name(argv[i])) != NULL) {
-					slot = q_vdb_pkg_eat(pkg_ctx, "SLOT");
-					if (strcmp(slotted_item, slot) != 0)
-						goto next_cat;
-				}
-			}
-			if (just_pkgname) {
-				if (dups_only) {
-					pkgname = atom_explode(de[x]->d_name);
-					snprintf(buf, buflen, "%s/%s", cat[j]->d_name, pkgname->P);
-					snprintf(swap, sizeof(swap), "%s/%s", cat[j]->d_name, pkgname->PN);
-					sets = add_set(swap, buf, sets);
-					atom_implode(pkgname);
-					goto next_cat;
-				}
-				pkgname = (verbose ? NULL : atom_explode(de[x]->d_name));
-				if ((qlist_all + just_pkgname) < 2) {
-
-					slot = NULL;
-
-					if (show_slots)
-						slot = q_vdb_pkg_eat(pkg_ctx, "SLOT");
-
-					/* display it */
-					printf("%s%s/%s%s%s%s%s%s%s%s%s%s\n", BOLD, cat[j]->d_name, BLUE,
-						(!columns ? (pkgname ? pkgname->PN : de[x]->d_name) : pkgname->PN),
-						(columns ? " " : ""), (columns ? pkgname->PV : ""),
-						NORM, YELLOW, slot ? slot_separator : "", slot ? slot : "", NORM,
-						umapstr(show_umap, cat[j]->d_name, de[x]->d_name));
-				}
-				if (pkgname)
-					atom_implode(pkgname);
-
-				if (qlist_all == 0)
-					goto next_cat;
-			}
-
-			if (verbose > 1)
-				printf("%s%s/%s%s%s\n%sCONTENTS%s:\n", BOLD, cat[j]->d_name, BLUE, de[x]->d_name, NORM, DKBLUE, NORM);
-
-			fp = q_vdb_pkg_fopenat_ro(pkg_ctx, "CONTENTS");
-			if (fp == NULL)
-				goto next_cat;
-
-			while (getline(&buf, &buflen, fp) != -1) {
-				contents_entry *e;
-
-				e = contents_parse_line(buf);
-				if (!e)
-					continue;
-
-				switch (e->type) {
-					case CONTENTS_DIR:
-						if (show_dir)
-							printf("%s%s%s/\n", verbose > 1 ? YELLOW : "" , e->name, verbose > 1 ? NORM : "");
-						break;
-					case CONTENTS_OBJ:
-						if (show_obj)
-							printf("%s%s%s\n", verbose > 1 ? WHITE : "" , e->name, verbose > 1 ? NORM : "");
-						break;
-					case CONTENTS_SYM:
-						if (show_sym) {
-							if (verbose)
-								printf("%s%s -> %s%s\n", verbose > 1 ? CYAN : "", e->name, e->sym_target, NORM);
-							else
-								printf("%s\n", e->name);
-						}
-						break;
-				}
-			}
-			fclose(fp);
-
- next_cat:
-			q_vdb_close_pkg(pkg_ctx);
-		}
-
-		q_vdb_close_cat(cat_ctx);
-		scandir_free(de, a);
-	}
-
-	if (dups_only) {
+	if (state.dups_only) {
 		char last[126];
-		queue *ll, *dups = filter_dups(sets);
+		depend_atom *atom;
+		queue *ll, *dups = filter_dups(state.sets);
 		last[0] = 0;
 
 		for (ll = dups; ll != NULL; ll = ll->next) {
@@ -364,24 +356,20 @@ int qlist_main(int argc, char **argv)
 			strncpy(last, atom->PN, sizeof(last));
 			if (ok)	{
 				char *slot = NULL;
-				if (show_slots)
+				if (state.show_slots)
 					slot = grab_vdb_item("SLOT", atom->CATEGORY, atom->P);
 				printf("%s%s/%s%s%s%s%s%s%s%s%s", BOLD, atom->CATEGORY, BLUE,
-					(!columns ? (verbose ? atom->P : atom->PN) : atom->PN), (columns ? " " : ""), (columns ? atom->PV : ""),
-					NORM, YELLOW, slot ? slot_separator : "", slot ? slot : "", NORM);
-				puts(umapstr(show_umap, atom->CATEGORY, atom->P));
+					(!state.columns ? (verbose ? atom->P : atom->PN) : atom->PN),
+					(state.columns ? " " : ""), (state.columns ? atom->PV : ""),
+					NORM, YELLOW, slot ? state.slot_separator : "", slot ? slot : "", NORM);
+				puts(umapstr(state.show_umap, atom->CATEGORY, atom->P));
 			}
 			atom_implode(atom);
 		}
 		free_sets(dups);
-		free_sets(sets);
+		free_sets(state.sets);
 	}
 
-	scandir_free(cat, dfd);
-
-	ret = EXIT_SUCCESS;
- done:
-	q_vdb_close(ctx);
 	return ret;
 }
 
