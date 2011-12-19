dc8cbe3ded08560507405b3bea8c84b62843c0fd
Mike Frysinger <vapier@gentoo.org>
2011-12-19 04:41:58 +0000

    fix -Wtype-limits warnings ... fwrite() cannot be less than 0

--- libq/safe_io.c
+++ libq/safe_io.c
@@ -3,7 +3,7 @@
  *
  * Copyright 2005-2010 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
- * $Header: /var/cvsroot/gentoo-projects/portage-utils/libq/safe_io.c,v 1.2 2010/01/13 19:37:39 vapier Exp $
+ * $Header: /var/cvsroot/gentoo-projects/portage-utils/libq/safe_io.c,v 1.3 2011/12/19 04:41:58 vapier Exp $
  */
 
 #include <stdio.h>
@@ -17,11 +17,15 @@ size_t safe_fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream)
 		this_ret = fwrite(ptr, size, nmemb, stream);
 		if (this_ret == nmemb)
 			return this_ret; /* most likely behavior */
-		if (this_ret < 0) {
-			if (errno == EAGAIN || errno == EINTR)
-				continue;
-			errp("fwrite(%p, %zu, %zu) failed (wrote %zu elements)",
-				ptr, size, nmemb, ret);
+		if (this_ret == 0) {
+			if (feof(stream))
+				break;
+			if (ferror(stream)) {
+				if (errno == EAGAIN || errno == EINTR)
+					continue;
+				errp("fwrite(%p, %zu, %zu) failed (wrote %zu elements)",
+					ptr, size, nmemb, ret);
+			}
 		}
 		nmemb -= this_ret;
 		ret += this_ret;
