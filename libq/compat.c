6baa58541b51e1805bc18c859a4b1d2b36b7d107
Mike Frysinger <vapier@gentoo.org>
2016-02-13 17:56:03 -0500

    compat: drop strcasestr/asprintf fallback
    We pull in strcasestr & asprintf via gnulib now, so don't need these
    local copies.

--- libq/compat.c
+++ libq/compat.c
@@ -8,28 +8,6 @@
 
 #define S_BLKSIZE DK_DEVID_BLKSIZE
 
-/* strcasestr is a GNU extention */
-char* strcasestr(const char *big, const char *little) {
-	char* b = alloca((strlen(big) + 1) * sizeof(char));
-	char* l = alloca((strlen(little) + 1) * sizeof(char));
-	char* off;
-	size_t i;
-	for (i = 0; big[i]; i++) b[i] = (char)tolower(big[i]);
-	for (i = 0; little[i]; i++) l[i] = (char)tolower(little[i]);
-	off = strstr(b, l);
-	return(off == NULL ? off : (char*)(big + (off - b)));
-}
-
-#undef  xasprintf
-#define xasprintf(strp, fmt, args...) \
-	do { /* xasprintf() */ \
-		char str[BUFSIZ]; \
-		if ((snprintf(str, sizeof(str)-1, fmt , ## args)) == -1) \
-			err("Out of stack space?"); \
-		str[sizeof(str)-1] = '\0'; \
-		*strp = xstrdup(str); \
-	} while (0)
-
 #elif defined(__hpux__) || defined(__MINT__)
 	/* must not include both dir.h and dirent.h on hpux11..11 & FreeMiNT */
 #elif defined(__linux__)
