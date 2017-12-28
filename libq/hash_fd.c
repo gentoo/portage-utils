913d23e85ae155edf6f9d957ecc6e938caf91683
Fabian Groffen <grobian@gentoo.org>
2017-12-28 17:56:59 +0100

    hash_fd: ensure __BYTE_ORDER is set
    Fallback to using sys/param.h when __BYTE_ORDER isn't set (which it
    isn't on Darwin at least)

--- libq/hash_fd.c
+++ libq/hash_fd.c
@@ -28,6 +28,19 @@
 #include <string.h>
 #include <unistd.h>
 
+#ifdef HAVE_SYS_PARAM_H
+# include <sys/param.h>
+# ifndef __BYTE_ORDER
+#  define __BYTE_ORDER BYTE_ORDER
+# endif
+# ifndef __BIG_ENDIAN
+#  define __BIG_ENDIAN BIG_ENDIAN
+# endif
+# ifndef __LITTLE_ENDIAN
+#  define __LITTLE_ENDIAN LITTLE_ENDIAN
+# endif
+#endif
+
 #include "busybox.h"
 
 #ifdef CONFIG_SHA1SUM
