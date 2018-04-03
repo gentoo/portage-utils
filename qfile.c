0baddddc111c7fba7a8a087a2905ca645a27f5f8
Fabian Groffen <grobian@gentoo.org>
2018-04-03 22:12:16 +0200

    qfile_main: use q_vdb_foreach_pkg_sorted, bug #607498
    For consistency with almost all other output, use sorted output variant
    for qfile as well.  The little sorting/memory overhead due to this is
    probably ok, given how much it is used already.
    
    Bug: https://bugs.gentoo.org/607498

--- qfile.c
+++ qfile.c
@@ -462,7 +462,7 @@ int qfile_main(int argc, char **argv)
 	nb_of_queries = prepare_qfile_args(argc, (const char **) argv, &state);
 	/* Now do the actual `qfile` checking */
 	if (nb_of_queries > 0)
-		found += q_vdb_foreach_pkg(qfile_cb, &state, NULL);
+		found += q_vdb_foreach_pkg_sorted(qfile_cb, &state);
 
 	if (state.args.non_orphans) {
 		/* display orphan files */
