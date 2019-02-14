ff05a366f0c27f6232fec29439ad3146e66b263c
Sam Besselink <sambesselink@planet.nl>
2019-02-14 01:22:31 +0000

    Define state globally as qcheck_state, enabling broader qcprintf() usage.

--- qcheck.c
+++ qcheck.c
@@ -33,7 +33,7 @@ static const char * const qcheck_opts_help[] = {
 };
 #define qcheck_usage(ret) usage(ret, QCHECK_FLAGS, qcheck_long_opts, qcheck_opts_help, NULL, lookup_applet_idx("qcheck"))
 
-#define qcprintf(fmt, args...) do { if (!state->bad_only) printf(_(fmt), ## args); } while (0)
+#define qcprintf(fmt, args...) do { if (!qcheck_state.bad_only) printf(_(fmt), ## args); } while (0)
 
 struct qcheck_opt_state {
 	array_t *atoms;
@@ -47,8 +47,11 @@ struct qcheck_opt_state {
 	bool undo_prelink;
 };
 
+/* Define globally, enabling broader qcprintf() usage */
+static struct qcheck_opt_state qcheck_state;
+
 static int
-qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
+qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx)
 {
 	int fd_contents;
 	FILE *fp_contents, *fp_contents_update;
@@ -79,9 +82,9 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 	/* Open contents_update, if needed */
 	num_files = num_files_ok = num_files_unknown = num_files_ignored = 0;
 	qcprintf("%sing %s%s/%s%s ...\n",
-		(state->qc_update ? "Updat" : "Check"),
+		(qcheck_state.qc_update ? "Updat" : "Check"),
 		GREEN, catname, pkgname, NORM);
-	if (state->qc_update) {
+	if (qcheck_state.qc_update) {
 		fp_contents_update = q_vdb_pkg_fopenat_rw(pkg_ctx, "CONTENTS~");
 		if (fp_contents_update == NULL) {
 			fclose(fp_contents);
@@ -90,7 +93,7 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 		}
 	}
 
-	if (!state->chk_config_protect) {
+	if (!qcheck_state.chk_config_protect) {
 		makeargv(config_protect, &cp_argc, &cp_argv);
 		makeargv(config_protect_mask, &cpm_argc, &cpm_argv);
 	}
@@ -108,13 +111,13 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 
 		/* run initial checks */
 		++num_files;
-		if (array_cnt(state->regex_arr)) {
+		if (array_cnt(qcheck_state.regex_arr)) {
 			size_t n;
 			regex_t *regex;
-			array_for_each(state->regex_arr, n, regex)
+			array_for_each(qcheck_state.regex_arr, n, regex)
 				if (!regexec(regex, entry->name, 0, NULL, 0))
 					break;
-			if (n < array_cnt(state->regex_arr)) {
+			if (n < array_cnt(qcheck_state.regex_arr)) {
 				--num_files;
 				++num_files_ignored;
 				continue;
@@ -122,7 +125,7 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 		}
 		if (fstatat(pkg_ctx->cat_ctx->ctx->portroot_fd, entry->name + 1, &st, AT_SYMLINK_NOFOLLOW)) {
 			/* make sure file exists */
-			if (state->chk_afk) {
+			if (qcheck_state.chk_afk) {
 				if (errno == ENOENT)
 					qcprintf(" %sAFK%s: %s\n", RED, NORM, entry->name);
 				else
@@ -130,14 +133,14 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 			} else {
 				--num_files;
 				++num_files_ignored;
-				if (state->qc_update)
+				if (qcheck_state.qc_update)
 					fputs(buffer, fp_contents_update);
 			}
 			continue;
 		}
 
 		/* Handle CONFIG_PROTECT-ed files */
-		if (!state->chk_config_protect) {
+		if (!qcheck_state.chk_config_protect) {
 			int i;
 			/* If in CONFIG_PROTECT_MASK, handle like normal */
 			for (i = 1; i < cpm_argc; ++i)
@@ -155,17 +158,17 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 
 		/* For certain combinations of flags and filetypes, a file
 		 * won't get checks and should be ignored */
-		if (!state->chk_mtime && entry->type == CONTENTS_SYM) {
+		if (!qcheck_state.chk_mtime && entry->type == CONTENTS_SYM) {
 			--num_files;
 			++num_files_ignored;
-			if (state->qc_update)
+			if (qcheck_state.qc_update)
 				fputs(buffer, fp_contents_update);
 
 			continue;
 		}
 
 		/* Digest checks only work on regular files
-		 * Note: We don't check for state->chk_hash when entering
+		 * Note: We don't check for state.chk_hash when entering
 		 * but rather in digest-check #3, because we only succeed
 		 * tests/qcheck/list04.good if when chk_hash is false, we
 		 * do check hashes, but only print mismatched digests as
@@ -183,19 +186,19 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 			}
 
 			if (!hash_algo) {
-				if (state->chk_hash) {
+				if (qcheck_state.chk_hash) {
 					qcprintf(" %sUNKNOWN DIGEST%s: '%s' for '%s'\n", RED, NORM, entry->digest, entry->name);
 					++num_files_unknown;
 				} else {
 					--num_files;
 					++num_files_ignored;
-					if (state->qc_update)
+					if (qcheck_state.qc_update)
 						fputs(buffer, fp_contents_update);
 				}
 				continue;
 			}
 
-			hash_cb_t hash_cb = state->undo_prelink ? hash_cb_prelink_undo : hash_cb_default;
+			hash_cb_t hash_cb = qcheck_state.undo_prelink ? hash_cb_prelink_undo : hash_cb_default;
 			f_digest = (char *)hash_file_at_cb(pkg_ctx->cat_ctx->ctx->portroot_fd, entry->name + 1, hash_algo, hash_cb);
 
 			/* Digest-check 2/3:
@@ -204,7 +207,7 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 				++num_files_unknown;
 				free(f_digest);
 
-				if (state->qc_update)
+				if (qcheck_state.qc_update)
 					fputs(buffer, fp_contents_update);
 
 				if (verbose)
@@ -216,8 +219,8 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 			/* Digest-check 3/3:
 			 * Does the digest equal what portage recorded? */
 			if (strcmp(entry->digest, f_digest) != 0) {
-				if (state->chk_hash) {
-					if (state->qc_update)
+				if (qcheck_state.chk_hash) {
+					if (qcheck_state.qc_update)
 						fprintf(fp_contents_update, "obj %s %s %"PRIu64"\n", entry->name, f_digest, (uint64_t)st.st_mtime);
 
 					const char *digest_disp;
@@ -234,7 +237,7 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 				} else {
 					--num_files;
 					++num_files_ignored;
-					if (state->qc_update)
+					if (qcheck_state.qc_update)
 						fputs(buffer, fp_contents_update);
 				}
 
@@ -246,14 +249,14 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 		}
 
 		/* Validate mtimes */
-		if (state->chk_mtime && entry->mtime && entry->mtime != st.st_mtime) {
+		if (qcheck_state.chk_mtime && entry->mtime && entry->mtime != st.st_mtime) {
 			qcprintf(" %sMTIME%s: %s", RED, NORM, entry->name);
 			if (verbose)
 				qcprintf(" (recorded '%"PRIu64"' != actual '%"PRIu64"')", (uint64_t)entry->mtime, (uint64_t)st.st_mtime);
 			qcprintf("\n");
 
 			/* Update mtime */
-			if (state->qc_update) {
+			if (qcheck_state.qc_update) {
 				if (entry->type == CONTENTS_SYM) {
 					fprintf(fp_contents_update, "sym %s -> %s %"PRIu64"\n", entry->name, entry->sym_target, (uint64_t)st.st_mtime);
 				} else {
@@ -265,7 +268,7 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 		}
 
 		/* Success! */
-		if (state->qc_update)
+		if (qcheck_state.qc_update)
 			fputs(buffer, fp_contents_update);
 
 		num_files_ok++;
@@ -274,12 +277,12 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 	free(buffer);
 	fclose(fp_contents);
 
-	if (!state->chk_config_protect) {
+	if (!qcheck_state.chk_config_protect) {
 		freeargv(cp_argc, cp_argv);
 		freeargv(cpm_argc, cpm_argv);
 	}
 
-	if (state->qc_update) {
+	if (qcheck_state.qc_update) {
 		if (fchown(fd_contents, cst.st_uid, cst.st_gid)) {
 			/* meh */;
 		}
@@ -292,7 +295,7 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 		if (!verbose)
 			return EXIT_SUCCESS;
 	}
-	if (state->bad_only && num_files_ok != num_files) {
+	if (qcheck_state.bad_only && num_files_ok != num_files) {
 		if (verbose)
 			printf("%s/%s\n", catname, pkgname);
 		else {
@@ -330,20 +333,19 @@ qcheck_process_contents(q_vdb_pkg_ctx *pkg_ctx, struct qcheck_opt_state *state)
 static int
 qcheck_cb(q_vdb_pkg_ctx *pkg_ctx, void *priv)
 {
-	struct qcheck_opt_state *state = priv;
 	const char *catname = pkg_ctx->cat_ctx->name;
 	const char *pkgname = pkg_ctx->name;
 	bool showit = false;
 
 	/* see if this cat/pkg is requested */
-	if (array_cnt(state->atoms)) {
+	if (array_cnt(qcheck_state.atoms)) {
 		char *buf;
 		size_t i;
 		depend_atom *qatom, *atom;
 
 		xasprintf(&buf, "%s/%s", catname, pkgname);
 		qatom = atom_explode(buf);
-		array_for_each(state->atoms, i, atom)
+		array_for_each(qcheck_state.atoms, i, atom)
 			if (atom_compare(atom, qatom) == EQUAL) {
 				showit = true;
 				break;
@@ -353,7 +355,7 @@ qcheck_cb(q_vdb_pkg_ctx *pkg_ctx, void *priv)
 	} else
 		showit = true;
 
-	return showit ? qcheck_process_contents(pkg_ctx, priv) : 0;
+	return showit ? qcheck_process_contents(pkg_ctx) : 0;
 }
 
 int qcheck_main(int argc, char **argv)
@@ -363,17 +365,16 @@ int qcheck_main(int argc, char **argv)
 	DECLARE_ARRAY(regex_arr);
 	depend_atom *atom;
 	DECLARE_ARRAY(atoms);
-	struct qcheck_opt_state state = {
-		.atoms = atoms,
-		.regex_arr = regex_arr,
-		.bad_only = false,
-		.qc_update = false,
-		.chk_afk = true,
-		.chk_hash = true,
-		.chk_mtime = true,
-		.chk_config_protect = true,
-		.undo_prelink = false,
-	};
+
+	qcheck_state.atoms = atoms;
+	qcheck_state.regex_arr = regex_arr;
+	qcheck_state.bad_only = false;
+	qcheck_state.qc_update = false;
+	qcheck_state.chk_afk = true;
+	qcheck_state.chk_hash = true;
+	qcheck_state.chk_mtime = true;
+	qcheck_state.chk_config_protect = true;
+	qcheck_state.undo_prelink = false;
 
 	while ((ret = GETOPT_LONG(QCHECK, qcheck, "")) != -1) {
 		switch (ret) {
@@ -384,13 +385,13 @@ int qcheck_main(int argc, char **argv)
 			xarraypush(regex_arr, &regex, sizeof(regex));
 			break;
 		}
-		case 'u': state.qc_update = true; break;
-		case 'A': state.chk_afk = false; break;
-		case 'B': state.bad_only = true; break;
-		case 'H': state.chk_hash = false; break;
-		case 'T': state.chk_mtime = false; break;
-		case 'P': state.chk_config_protect = false; break;
-		case 'p': state.undo_prelink = prelink_available(); break;
+		case 'u': qcheck_state.qc_update = true; break;
+		case 'A': qcheck_state.chk_afk = false; break;
+		case 'B': qcheck_state.bad_only = true; break;
+		case 'H': qcheck_state.chk_hash = false; break;
+		case 'T': qcheck_state.chk_mtime = false; break;
+		case 'P': qcheck_state.chk_config_protect = false; break;
+		case 'p': qcheck_state.undo_prelink = prelink_available(); break;
 		}
 	}
 
@@ -404,7 +405,7 @@ int qcheck_main(int argc, char **argv)
 			xarraypush_ptr(atoms, atom);
 	}
 
-	ret = q_vdb_foreach_pkg_sorted(qcheck_cb, &state);
+	ret = q_vdb_foreach_pkg_sorted(qcheck_cb, &qcheck_state);
 	{
 		void *regex;
 		array_for_each(regex_arr, i, regex)
