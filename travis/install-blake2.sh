a2d1f0c672088831531c92508f8e4bf6a3e6480c
Fabian Groffen <grobian@gentoo.org>
2019-05-21 16:28:11 +0200

    travis: attempt to install blake2
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

---
+++ travis/install-blake2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+. "${0%/*}"/lib.sh
+
+main() {
+	local pv="0.98.1"
+	local S="libb2-${pv}"
+	travis_fold start dep-blake2
+	rm -rf libb2*
+	v mkdir -p ../sysroot
+	v wget https://github.com/BLAKE2/libb2/releases/download/v${pv}/libb2-${pv}.tar.gz
+	v tar xf libb2-${pv}.tar.gz
+	(
+		cd "${S}"
+		./configure \
+			--enable-static \
+			--disable-shared \
+			--prefix=/ \
+			--libdir=/ \
+			--includedir=/
+		m
+		m DESTDIR=../../sysroot install
+	)
+	v rm -rf libb2*
+	travis_fold end dep-blake2
+}
+main "$@"
