60232a12416e58fdae27d97cf9c62d95659a7db4
Fabian Groffen <grobian@gentoo.org>
2019-07-14 13:47:55 +0200

    qatom: fix atom matching wrt command line arguments
    ensure we match things without category or with expressions
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- qsize.c
+++ qsize.c
@@ -113,7 +113,7 @@ qsize_cb(tree_pkg_ctx *pkg_ctx, void *priv)
 
 		qatom = tree_get_atom(pkg_ctx, 0);
 		array_for_each(state->atoms, i, atom)
-			if (atom_compare(atom, qatom) == EQUAL) {
+			if (atom_compare(qatom, atom) == EQUAL) {
 				showit = true;
 				break;
 			}
