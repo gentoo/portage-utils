5cdb5572185636cd5ab056241635d33b1b85fc98
Fabian Groffen <grobian@gentoo.org>
2020-01-19 20:18:48 +0100

    libq/tree: help Coverity 206538 String not null terminated
    Coverity doesn't see that zalloc would have null-terminated.  Upside,
    replace zalloc with malloc and just clear one (relevant) byte.
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- libq/tree.c
+++ libq/tree.c
@@ -689,10 +689,11 @@ tree_read_file_md5(tree_pkg_ctx *pkg_ctx)
 		goto err;
 
 	len = sizeof(*ret) + s.st_size + 1;
-	ret = xzalloc(len);
+	ret = xmalloc(len);
 	ptr = (char*)ret + sizeof(*ret);
 	if ((off_t)fread(ptr, 1, s.st_size, f) != s.st_size)
 		goto err;
+	ptr[s.st_size] = '\0';
 
 	/* We have a block of key=value\n data.
 	 * KEY=VALUE\n
