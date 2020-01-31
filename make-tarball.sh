214fac0f5b899dbb40f7932f903a2aad4d42f161
Fabian Groffen <grobian@gentoo.org>
2020-01-31 14:26:26 +0100

    make-tarball: ensure up-to-date manpages
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- make-tarball.sh
+++ make-tarball.sh
@@ -27,9 +27,12 @@ git) ver="HEAD" ;;
 	;;
 esac
 p="${TMPDIR:-/var/tmp}/portage-utils-${ver#v}"
+pb=${p}_build
 
 rm -rf "${p}"
 mkdir "${p}"
+rm -rf "${pb}"
+mkdir "${pb}"
 
 einfo "Checking out clean git sources ..."
 git archive "${ver}" | tar xf - -C "${p}"
@@ -42,23 +45,24 @@ sed -i "/^AM_MAINTAINER_MODE/s:(.*)$::" configure.ac
 rm -rf autom4te.cache
 popd >/dev/null
 
+einfo "Checking sources ..."
+pushd "${pb}" >/dev/null
+tar cf - -C "${p}" . | tar xf -
+v ./configure -q
+v ${MAKE} -s
+v ${MAKE} -s check
+v ${MAKE} -s DESTDIR="${PWD}"/install install
+v ./man/mkman.py
+# ensure the tar contains up-to-date manpages
+cp -a man/*.1 "${p}"/man
+popd >/dev/null
+rm -rf "${pb}"
+
 einfo "Generating tarball ..."
 pushd "${p%/*}" >/dev/null
 tar --numeric-owner -cf - "${p##*/}" | xz > "${p}".tar.xz
 popd >/dev/null
 rm -r "${p}"
-
-einfo "Checking tarball ..."
-pushd "${p%/*}" >/dev/null
-tar xf "${p}".tar.*
-popd >/dev/null
-pushd "${p}" >/dev/null
-v ./configure -q
-v ${MAKE} -s
-v ${MAKE} -s check
-v ${MAKE} -s DESTDIR="${PWD}"/install install
-popd >/dev/null
-rm -rf "${p}"
 
 echo
 einfo "All ready for distribution!"
