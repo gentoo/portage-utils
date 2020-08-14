gentoo/portage-utils

commit bafe11f5e8beb6ee1fe5e344aba517a22bc0cf9f

qmerge: Impl. best_version()
Signed-off-by: Joakim Tjernlund <joakim.tjernlund@infinera.com>
Signed-off-by: Fabian Groffen <grobian@gentoo.org>

qmerge.c
@@ -651,7 +651,7 @@ pkg_run_func_at(int dirfd, const char *vdb_path, const char *phases, const char 
 		"debug-print-section() { :; }\n"
 		/* Not quite right */
 		"has_version() { [ -n \"$(qlist -ICqe \"$1\")\" ]; }\n"
-		/* best_version() */
+		"best_version() { qlist -ICqev \"$1\" }\n"
 		"use() { useq \"$@\"; }\n"
 		"usex() { useq \"$1\" && echo \"${2-yes}$4\" || echo \"${3-no}$5\"; }\n"
 		"useq() { hasq \"$1\" ${USE}; }\n"

commit d188469068509cc3f802b2f9f916b0f4daa911ac

qmerge: fix syntax error introduced in bafe11f5e8beb6ee1fe5e344aba517a22bc0cf9f
Signed-off-by: Fabian Groffen <grobian@gentoo.org>

qmerge.c
@@ -651,7 +651,7 @@ pkg_run_func_at(int dirfd, const char *vdb_path, const char *phases, const char 
 		"debug-print-section() { :; }\n"
 		/* Not quite right */
 		"has_version() { [ -n \"$(qlist -ICqe \"$1\")\" ]; }\n"
-		"best_version() { qlist -ICqev \"$1\" }\n"
+		"best_version() { qlist -ICqev \"$1\"; }\n"
 		"use() { useq \"$@\"; }\n"
 		"usex() { useq \"$1\" && echo \"${2-yes}$4\" || echo \"${3-no}$5\"; }\n"
 		"useq() { hasq \"$1\" ${USE}; }\n"