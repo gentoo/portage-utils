543c10d424a5965d85133ce8226ac8b8b7fdd6e2
Fabian Groffen <grobian@gentoo.org>
2026-01-10 10:47:10 +0100

    libq/xarray: drop DEFINE_ARRAY macro
    it feels somewhat misleading to just define a pointer to a struct via a
    macro, drop it
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- libq/xarray.h
+++ libq/xarray.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2003-2019 Gentoo Foundation
+ * Copyright 2003-2026 Gentoo Foundation
  * Distributed under the terms of the GNU General Public License v2
  *
  * Copyright 2003-2007 Ned Ludd        - <solar@gentoo.org>
@@ -33,7 +33,6 @@ typedef struct {
 #define array_init_decl { .eles = NULL, .num = 0, }
 #define array_cnt(arr) (arr)->num
 #define DECLARE_ARRAY(arr) array_t _##arr = array_init_decl, *arr = &_##arr
-#define DEFINE_ARRAY(arr) array_t *arr;
 #define xarraypush_str(arr, ele) xarraypush(arr, ele, strlen(ele) + 1 /*NUL*/)
 #define xarraypush_struct(arr, ele) xarraypush(arr, ele, sizeof(*(ele)))
 
