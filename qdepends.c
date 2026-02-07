4101984f7367d6730e69a38ced5db578d21d6e2c
Fabian Groffen <grobian@gentoo.org>
2026-02-07 15:36:21 +0100

    qdepends: enable all *DEPEND when none are given
    with a possible growing set of flags, just check for any of the specific
    *DEPEND flags being given, for it makes no sense to run without printing
    any
    
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- qdepends.c
+++ qdepends.c
@@ -334,8 +334,13 @@ int qdepends_main(int argc, char **argv)
 		}
 	}
 
-	if ((state.qmode & ~(QMODE_REVERSE | QMODE_INSTALLED | QMODE_TREE)) == 0) {
-		/* default mode of operation: -drpb (also for just -Q) */
+	if ((state.qmode & (QMODE_DEPEND  |
+						QMODE_RDEPEND |
+						QMODE_PDEPEND |
+						QMODE_BDEPEND |
+						QMODE_IDEPEND )) == 0)
+	{
+		/* default mode of printing: -drpb */
 		state.qmode |= QMODE_DEPEND  |
 					   QMODE_RDEPEND |
 					   QMODE_PDEPEND |
