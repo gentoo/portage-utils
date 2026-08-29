f74404c393aa9a2063c41b6313e081a40555351d
Fabian Groffen <grobian@gentoo.org>
2026-08-29 11:21:55 +0200

    libq/colors: reflow for 2026 codestyle
    Signed-off-by: Fabian Groffen <grobian@gentoo.org>

--- libq/colors.c
+++ libq/colors.c
@@ -15,15 +15,15 @@
 
 #define CPAIR_VALUE_LEN 16
 typedef struct {
-	const char  *name;
-	const char  *origval;
-	const char **var;
-	char         tmpbuf[CPAIR_VALUE_LEN];
+  const char  *name;
+  const char  *origval;
+  const char **var;
+  char         tmpbuf[CPAIR_VALUE_LEN];
 } colourpair;
 
 typedef struct {
-	const char *name;
-	const char *value;
+  const char *name;
+  const char *value;
 } colourmap;
 
 /* color constants */
@@ -69,171 +69,183 @@ static bool colour_mapped = false;
 
 /* all of the above need to be referenced in the list below */
 static colourpair colour_pairs[] = {
-	{"norm",       _MAKE_COLOUR2(0,0),   &NORM,      ""},
-	{"teal",       LQC_TEAL,             &BLUE,      ""},
-	{"bold",       _MAKE_COLOUR2(0,1),   &BOLD,      ""},
-	{"brown",      LQC_BROWN,            &YELLOW,    ""},
-	{"turquoise",  LQC_TURQUOISE,        &CYAN,      ""},
-	{"darkblue",   LQC_DARKBLUE,         &DKBLUE,    ""},
-	{"green",      LQC_GREEN,            &DKGREEN,   ""},
-	{"darkgreen",  LQC_DARKGREEN,        &GREEN,     ""},
-	{"fuchsia",    LQC_FUCHSIA,          &MAGENTA,   ""},
-	{"red",        LQC_DARKRED,          &RED,       ""},
-	{"white",      _MAKE_COLOUR2(1,38),  &WHITE,     ""},
-	{"yellow",     LQC_YELLOW,           &BRYELLOW,  ""}
+  {"norm",       _MAKE_COLOUR2(0,0),   &NORM,      ""},
+  {"teal",       LQC_TEAL,             &BLUE,      ""},
+  {"bold",       _MAKE_COLOUR2(0,1),   &BOLD,      ""},
+  {"brown",      LQC_BROWN,            &YELLOW,    ""},
+  {"turquoise",  LQC_TURQUOISE,        &CYAN,      ""},
+  {"darkblue",   LQC_DARKBLUE,         &DKBLUE,    ""},
+  {"green",      LQC_GREEN,            &DKGREEN,   ""},
+  {"darkgreen",  LQC_DARKGREEN,        &GREEN,     ""},
+  {"fuchsia",    LQC_FUCHSIA,          &MAGENTA,   ""},
+  {"red",        LQC_DARKRED,          &RED,       ""},
+  {"white",      _MAKE_COLOUR2(1,38),  &WHITE,     ""},
+  {"yellow",     LQC_YELLOW,           &BRYELLOW,  ""}
 };
 
 static colourmap colour_map[] = {
-	/* Portage's list of names */
-	{"black",      LQC_BLACK    },
-	{"darkgrey",   LQC_DARKGREY },
-	{"darkgray",   LQC_DARKGREY },
-	{"red",        LQC_RED      },
-	{"darkred",    LQC_DARKRED  },
-	{"green",      LQC_GREEN    },
-	{"darkgreen",  LQC_DARKGREEN},
-	{"yellow",     LQC_YELLOW   },
-	{"brown",      LQC_BROWN    },
-	{"darkyellow", LQC_BROWN    },
-	{"blue",       LQC_BLUE     },
-	{"darkblue",   LQC_DARKBLUE },
-	{"fuchsia",    LQC_FUCHSIA  },
-	{"purple",     LQC_PURPLE   },
-	{"turquoise",  LQC_TURQUOISE},
-	{"darkteal",   LQC_TURQUOISE},
-	{"teal",       LQC_TEAL     },
-	/* portage-utils historical colour names */
-	{"bryellow",   LQC_BROWN    },
-	{"cyan",       LQC_TURQUOISE},
-	{"dkblue",     LQC_DARKBLUE },
-	{"dkgreen",    LQC_GREEN    },
-	{"magenta",    LQC_FUCHSIA  }
+  /* Portage's list of names */
+  {"black",      LQC_BLACK    },
+  {"darkgrey",   LQC_DARKGREY },
+  {"darkgray",   LQC_DARKGREY },
+  {"red",        LQC_RED      },
+  {"darkred",    LQC_DARKRED  },
+  {"green",      LQC_GREEN    },
+  {"darkgreen",  LQC_DARKGREEN},
+  {"yellow",     LQC_YELLOW   },
+  {"brown",      LQC_BROWN    },
+  {"darkyellow", LQC_BROWN    },
+  {"blue",       LQC_BLUE     },
+  {"darkblue",   LQC_DARKBLUE },
+  {"fuchsia",    LQC_FUCHSIA  },
+  {"purple",     LQC_PURPLE   },
+  {"turquoise",  LQC_TURQUOISE},
+  {"darkteal",   LQC_TURQUOISE},
+  {"teal",       LQC_TEAL     },
+  /* portage-utils historical colour names */
+  {"bryellow",   LQC_BROWN    },
+  {"cyan",       LQC_TURQUOISE},
+  {"dkblue",     LQC_DARKBLUE },
+  {"dkgreen",    LQC_GREEN    },
+  {"magenta",    LQC_FUCHSIA  }
 };
 static colourmap rgb_map[] = {
-	/* RGB ANSI codes */
-	{"0x000000",   LQC_BLACK    },
-	{"0x555555",   LQC_DARKGREY },
-	{"0xAA0000",   LQC_RED      },
-	{"0xFF5555",   LQC_DARKRED  },
-	{"0x00AA00",   LQC_GREEN    },
-	{"0x55FF55",   LQC_DARKGREEN},
-	{"0xAA5500",   LQC_YELLOW   },
-	{"0xFFFF55",   LQC_BROWN    },
-	{"0x0000AA",   LQC_BLUE     },
-	{"0x5555FF",   LQC_DARKBLUE },
-	{"0xAA00AA",   LQC_FUCHSIA  },
-	{"0xFF55FF",   LQC_PURPLE   },
-	{"0x00AAAA",   LQC_TURQUOISE},
-	{"0x55FFFF",   LQC_TEAL     },
-	{"0xAAAAAA",   LQC_WHITE    },
-	{"0xFFFFFF",   LQC_LIGHTGREY},
-	/* some terminals have darkyellow instead of brown */
-	{"0xAAAA00",   LQC_BROWN    }
+  /* RGB ANSI codes */
+  {"0x000000",   LQC_BLACK    },
+  {"0x555555",   LQC_DARKGREY },
+  {"0xAA0000",   LQC_RED      },
+  {"0xFF5555",   LQC_DARKRED  },
+  {"0x00AA00",   LQC_GREEN    },
+  {"0x55FF55",   LQC_DARKGREEN},
+  {"0xAA5500",   LQC_YELLOW   },
+  {"0xFFFF55",   LQC_BROWN    },
+  {"0x0000AA",   LQC_BLUE     },
+  {"0x5555FF",   LQC_DARKBLUE },
+  {"0xAA00AA",   LQC_FUCHSIA  },
+  {"0xFF55FF",   LQC_PURPLE   },
+  {"0x00AAAA",   LQC_TURQUOISE},
+  {"0x55FFFF",   LQC_TEAL     },
+  {"0xAAAAAA",   LQC_WHITE    },
+  {"0xFFFFFF",   LQC_LIGHTGREY},
+  /* some terminals have darkyellow instead of brown */
+  {"0xAAAA00",   LQC_BROWN    }
 };
 
 void
 color_remap(void)
 {
-	FILE *fp;
-	unsigned int i;
-	int linelen;
-	size_t buflen;
-	char *buf;
-	char *p;
-	unsigned int lineno = 0;
+  FILE *fp;
+  unsigned int i;
+  int linelen;
+  size_t buflen;
+  char *buf;
+  char *p;
+  unsigned int lineno = 0;
 
-	/* (re)set to defaults */
-	for (i = 0; i < ARRAY_SIZE(colour_pairs); i++)
-		*(colour_pairs[i].var) = colour_pairs[i].origval;
+  /* (re)set to defaults */
+  for (i = 0; i < ARRAY_SIZE(colour_pairs); i++)
+    *(colour_pairs[i].var) = colour_pairs[i].origval;
 
-	if ((fp = fopen(COLOR_MAP, "r")) == NULL)
-		return;
+  if ((fp = fopen(COLOR_MAP, "r")) == NULL)
+    return;
 
-	buf = NULL;
-	while ((linelen = getline(&buf, &buflen, fp)) >= 0) {
-		lineno++;
-		/* eat comments */
-		if ((p = strchr(buf, '#')) != NULL)
-			*p = '\0';
+  buf = NULL;
+  while ((linelen = getline(&buf, &buflen, fp)) >= 0)
+  {
+    lineno++;
+    /* eat comments */
+    if ((p = strchr(buf, '#')) != NULL)
+      *p = '\0';
 
-		p = strchr(buf, '=');
-		if (p == NULL)
-			continue;
+    p = strchr(buf, '=');
+    if (p == NULL)
+      continue;
 
-		*p++ = '\0'; /* split the pair */
-		rmspace(buf);
-		rmspace(p);
+    *p++ = '\0'; /* split the pair */
+    rmspace(buf);
+    rmspace(p);
 
-		/* strip off quotes from values */
-		linelen = (int)strlen(p);
-		if (linelen > 1 &&
-			p[0] == p[linelen - 1] &&
-			(p[0] == '\'' ||
-			 p[0] == '"'))
-		{
-			p[linelen - 1] = '\0';
-			p++;
-		}
+    /* strip off quotes from values */
+    linelen = (int)strlen(p);
+    if (linelen > 1 &&
+        p[0] == p[linelen - 1] &&
+        (p[0] == '\'' ||
+         p[0] == '"'))
+    {
+      p[linelen - 1] = '\0';
+      p++;
+    }
 
-		for (i = 0; i < ARRAY_SIZE(colour_pairs); i++) {
-			int found = 0;
-			if (strcmp(buf, colour_pairs[i].name) == 0) {
-				if (strncmp(p, "0x", 2) == 0) {
-					size_t n;
-					for (n = 0; n < ARRAY_SIZE(rgb_map); n++) {
-						if (strcasecmp(rgb_map[n].name, p) == 0) {
-							found = 1;
-							*(colour_pairs[i].var) = rgb_map[n].value;
-							break;
-						}
-					}
-					if (found == 0)
-						warn("[%s=%s] arbitrary RGB values in color map "
-							 "are not supported on line %d of %s",
-							 buf, p, lineno, COLOR_MAP);
-				} else {
-					/* color=color format support */
-					size_t n;
-					for (n = 0; n < ARRAY_SIZE(colour_map); n++) {
-						if (strcasecmp(colour_map[n].name, p) == 0) {
-							found = 1;
-							*(colour_pairs[i].var) = colour_map[n].value;
-							break;
-						}
-					}
+    for (i = 0; i < ARRAY_SIZE(colour_pairs); i++)
+    {
+      int found = 0;
+      if (strcmp(buf, colour_pairs[i].name) == 0)
+      {
+        if (strncmp(p, "0x", 2) == 0) {
+          size_t n;
+          for (n = 0; n < ARRAY_SIZE(rgb_map); n++)
+          {
+            if (strcasecmp(rgb_map[n].name, p) == 0)
+            {
+              found = 1;
+              *(colour_pairs[i].var) = rgb_map[n].value;
+              break;
+            }
+          }
+          if (found == 0)
+            warn("[%s=%s] arbitrary RGB values in color map "
+                 "are not supported on line %d of %s",
+                 buf, p, lineno, COLOR_MAP);
+        }
+        else
+        {
+          /* color=color format support */
+          size_t n;
+          for (n = 0; n < ARRAY_SIZE(colour_map); n++)
+          {
+            if (strcasecmp(colour_map[n].name, p) == 0)
+            {
+              found = 1;
+              *(colour_pairs[i].var) = colour_map[n].value;
+              break;
+            }
+          }
 
-					if (found == 0) {
-						snprintf(colour_pairs[i].tmpbuf,
-								 sizeof(colour_pairs[i].tmpbuf),
-								 "\e[%s", p);
-						*(colour_pairs[i].var) = colour_pairs[i].tmpbuf;
-					}
-				}
+          if (found == 0)
+          {
+            snprintf(colour_pairs[i].tmpbuf,
+                     sizeof(colour_pairs[i].tmpbuf),
+                     "\e[%s", p);
+            *(colour_pairs[i].var) = colour_pairs[i].tmpbuf;
+          }
+        }
 
-				break;
-			}
-		}
-	}
+        break;
+      }
+    }
+  }
 
-	free(buf);
-	fclose(fp);
+  free(buf);
+  fclose(fp);
 
-	colour_mapped = true;
+  colour_mapped = true;
 }
 
 void
 color_clear(void)
 {
-	unsigned int i;
+  unsigned int i;
 
-	for (i = 0; i < ARRAY_SIZE(colour_pairs); i++)
-		*(colour_pairs[i].var) = "";
+  for (i = 0; i < ARRAY_SIZE(colour_pairs); i++)
+    *(colour_pairs[i].var) = "";
 
-	colour_mapped = false;
+  colour_mapped = false;
 }
 
 bool
 color_mapped(void)
 {
-	return colour_mapped;
+  return colour_mapped;
 }
+
+/* vim: set ts=2 sw=2 expandtab cino+=\:0 foldmethod=marker: */
